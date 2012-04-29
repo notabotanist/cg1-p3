9ac30ed9716c7eb7a07a157792e5a224e7717cc8
Matt MacEwan(Netbook) <mrm4677@cs.rit.edu>
2012-04-28 22:53:55 -0400

    Initial commit

---
+++ stereo.cpp
@@ -0,0 +1,47 @@
+// 
+// File: stereo.cpp 
+// 
+// @author matthew: TODO_AUTHOR_FULL_NAME_
+// 
+// Version: 
+// $Id$ 
+// 
+// Revisions: 
+// $Log$ 
+// 
+// //////////////////////////////////////////////////////////////////// 
+
+// include files, system headers first
+#include <iostream>         // system header include for I/O
+
+#include "stereo.h"          // local header include
+
+/// namespace TODO_OPTIONAL_ is optional
+// namespace TODO_OPTIONAL_ {
+
+/// ctor TODO_BRIEF_DOCS_
+stereo::stereo() : {
+}
+
+/// dtor TODO_BRIEF_DOCS_
+stereo::~stereo() {
+
+}
+
+// } // end namespace TODO_OPTIONAL_
+
+// (main function is NEVER in a namespace)
+/////////////////////////// main // TODO_ // DELETE section if not needed
+
+///
+/// main function TODO_DOCS_
+/// @author matthew: TODO_AUTHOR_FULL_NAME_
+///
+/// @param argc  number of command line arguments, including program name
+/// @param argv  supplied command line arguments, including program name
+/// @returns errorCode  the error Code; 0 if no error
+///
+int main( int argc, char* argv[] ) {
+
+}
+
---
+++ stereo.h
@@ -0,0 +1,23 @@
+// 
+// File: stereo.h 
+// 
+// stereo.h TODO_DOCS_
+// 
+// Version: 
+// $Id$ 
+// 
+// Revisions: 
+// $Log$ 
+// 
+// //////////////////////////////////////////////////////////////////// 
+
+#ifndef STEREO_H_
+#define STEREO_H_
+
+/// @author matthew: TODO_AUTHOR_FULL_NAME_
+///
+
+//  replace with useful content 
+
+#endif // STEREO_H_
+
