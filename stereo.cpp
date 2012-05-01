2a81277c55ec0d6dcc91d44b8a02a1f3e1b205f5
Matt MacEwan(Netbook) <mrm4677@cs.rit.edu>
2012-05-01 01:08:30 -0400

    Stereo-type rendering is go!
    I'd been drawing the cube behind the camera this whole time -_-;

--- stereo.cpp
+++ stereo.cpp
@@ -25,38 +25,37 @@ StereoViewport::StereoViewport() : width(1000), height(500), x(0), y(0), eyeoff(
 
 /// rendering callback
 void StereoViewport::display() {
+	glClear(GL_COLOR_BUFFER_BIT);
 
 	// setup left viewport (right eye view)
-	glViewport(x, y, width/2, height);
+	glViewport(x, y, width/2.0, height);
 	// set up projection matrix
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60.0, (GLfloat)width / (GLfloat)(2 * height), 1.0, 20.0);
 	glMatrixMode(GL_MODELVIEW);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	glTranslatef(-eyeoff, 0, 0);
 	drawScene();
-	/*
 	// setup right viewport (left eye view)
-	glViewport(x+width/2, y, width/2, height);
+	glViewport(x+width/2.0, y, width/2.0, height);
 	// set up projection matrix
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60.0, (GLfloat)width / (GLfloat)(2 * height), 1.0, 20.0);
 	glMatrixMode(GL_MODELVIEW);
+	glClear(GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	glTranslatef(eyeoff, 0, 0);
-	glClear(GL_COLOR_BUFFER_BIT);
 	drawScene();
-	*/
 
 	glFlush();
 }
 
 /// default scene
 void StereoViewport::drawScene() {
-	glTranslatef(0.0, 0.0, 5);
+	glTranslatef(0.0, 0.0, -5);
 	glColor3f(1, 1, 1);
 	glutWireCube(1.0);
 }
@@ -81,7 +80,7 @@ static void gldisplay() {
 ///
 int main( int argc, char* argv[] ) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
 	glutInitWindowSize(1000, 500);
 	glutCreateWindow(argv[0]);
 
