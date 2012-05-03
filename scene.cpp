84196cc3223aa5cd7280ac5a12012dd7be684d86
Matt MacEwan(Desk) <mrm4677@rit.edu>
2012-05-03 00:59:49 -0400

    First steps of scene management system
    Need a new makefile.

---
+++ scene.cpp
@@ -0,0 +1,27 @@
+//
+// File: scene.cpp
+//
+// ///////////////////////////////////////////////////////////////////// 
+
+#include "scene.h"
+#include <GL/glut.h>
+
+Geometry::Geometry() {
+}
+
+Scene::Scene() : children() {
+}
+
+void Scene::render() {
+	// store current matrix
+	glPushMatrix();
+
+	for (vector<Geometry&>::iterator iter(children.begin());
+			iter != children.end(); iter++) {
+		glPopMatrix();
+		glPushMatrix();
+		iter->render();
+	}
+
+	glPopMatrix();
+}
---
+++ scene.h
@@ -0,0 +1,39 @@
+//
+// File: scene.h
+//
+// Classes for organizing and drawing a scene
+// ///////////////////////////////////////////////////////////////////// 
+
+#ifndef SCENE_H_
+#define SCENE_H_
+
+#include <vector>
+
+/// @author mrm4677
+/// Abstract base class for objects renderable in Scenes
+class Geometry {
+protected:
+	Geometry();	// protected to make class abstract
+public:
+	virtual ~Geometry() {}
+
+	/// render as implementor sees fit
+	void render() = 0;
+};
+
+/// @author mrm4677
+/// Container for Geometries
+class Scene {
+private:
+	/// can't instantiate Geometry objects. Does STL work with refs?
+	vector<Geometry&> children;
+
+public:
+	Scene();
+	virtual ~Scene() {}
+
+	/// Iterates over all child Geometries and renders them in turn
+	void render();
+}
+
+#endif
